fbbe97ed0824af672cb35d883415c8f13b8a6ead
Yash Dave <yash@deepsource.io>
2022-07-19 16:11:24 +0530

    feat: init vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vitepress";
+export default defineConfig({
+  title: "VitePress",
+  titleTemplate: "Amorpheuz's exploration",
+  description: "Just playing around.",
+  lang: "en-US",
+  lastUpdated: true,
+});
---
+++ docs/getting-started.md
@@ -0,0 +1,5 @@
+---
+layout: doc
+---
+
+# Let's get started with Vitepress
